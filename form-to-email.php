c4b2f3cfe27238d997a696193a0b47ce9e9d8d19
Annie4Star <annmarie.foerster@gmail.com>
2019-11-29 02:37:46 -0500

    Worked on forms again, some css minify

--- form-to-email.php
+++ form-to-email.php
@@ -1,41 +1,44 @@
 <?php
-if(!isset($_POST['submit']))
+if(!isset($_POST['send']))
 {
 	//This page should not be accessed directly. Need to submit the form.
 	echo "error; you need to submit the form!";
 }
+{
 $first_name = $_POST['first_name'];
 $last_name = $_POST['last_name'];
 $business = $_POST['business'];
 $email = $_POST['email'];
 $message = $_POST['message'];
+$submit = $_POST['submit'];
+}
 
 //Validate first
-if(empty($first_name)||empty($last_name)||empty($email)) 
-{
-    echo "First and last name and email are mandatory!";
-    exit;
-}
+//if(empty($first_name)||empty($last_name)||empty($email)) 
+//{
+//    echo "First and last name and email are mandatory!";
+//    exit;
+//}
 
-if(IsInjected($email))
-{
-    echo "Bad email value!";
-    exit;
-}
+//if(IsInjected($email))
+//{
+//    echo "Bad email value!";
+//    exit;
+//}
 
-$email_from = 'admin@annmariefoerster.com';//<== update the email address
-$email_subject = "New Form submission";
-$email_body = "You have received a new message from $first_name.\n".
+{$email_from = 'admin@annmariefoerster.com';//<== update the email address
+$subject = "New Form submission";
+$email_body = "You have received a new message from $first_name at $email.\n".
     "Here is the message:\n $message".
     
 $to = "admin@annmariefoerster.com";//<== update the email address
-$headers = "From: $email_from \r\n";
+$headers = "From: $email \r\n";
 $headers .= "Reply-To: $email \r\n";
 //Send the email!
-mail($to,$email_subject,$email_body,$headers);
+mail( $to,$subject,$body,$headers);
 //done. redirect to thank-you page.
 header('Location: thank-you.html');
-
+}
 
 // Function to validate against any email injection attempts
 function IsInjected($str)
---
+++ js/foerfolio.js
@@ -0,0 +1,5 @@
+function emailE {
+    emailE='annmariefoerster.com'
+    emailE=('admin' + '@' + emailE)
+    document.write('<a href="mailto:' + emailE + '">' + 'Email me directly.' + '</a>')
+}
