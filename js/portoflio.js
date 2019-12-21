f079a266aea1f761844504586d6d428f9be80aaf
Ann Foerster <annmarie.foerster@gmail.com>
2019-12-20 22:31:34 -0500

    Changed from Uploading

--- js/portoflio.js
+++ js/portoflio.js
@@ -20,35 +20,3 @@ function respFunction() {
       x.style.display = "block";
     }
   }
-
-$('#contact_form').submit(function(e) {
-    e.preventDefault();
-    var first_name = $('#first_name').val();
-    var last_name = $('#last_name').val();
-    var email = $('#email').val();
-	var message  = $('#message').val();
-     
-    $(".error").remove();
- 
-    if (first_name.length < 1) {
-      $('#first_name').after('<span class="error">This field is required</span>');
-    }
-    if (last_name.length < 1) {
-      $('#last_name').after('<span class="error">This field is required</span>');
-    }
-    if (email.length < 1) {
-      $('#email').after('<span class="error">This field is required</span>');
-    } else {
-      var regEx = /^[A-Z0-9][A-Z0-9._%+-]{0,63}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/;
-      var validEmail = regEx.test(email);
-      if (!validEmail) {
-        $('#email').after('<span class="error">Enter a valid email</span>');
-      }
-    }
-  });
-  document.getElementById("contact_form").onsubmit = function() {success()};
-
- function success() {
-     alert("Thank you for contacting me.  I'll be with you shortly.");
-     document.forms["contact_form"].reset();	 
- }
