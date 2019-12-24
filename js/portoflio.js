03e7ac58bb2a903352d2d70394e41803c9a82ae9
Ann Foerster <annmarie.foerster@gmail.com>
2019-12-24 14:12:05 -0500

    schema

--- js/portoflio.js
+++ js/portoflio.js
@@ -7,9 +7,9 @@ function coverMail() {
 	emailE=('admin' + '@' + emailE)
 }
 
-document.getElementById("theFooter").innerHTML = d.getFullYear() + "  The site is by Ann-Marie Foerster.  " + '<a href="mailto:' + emailE + '">' + 'Contact me' + '</a>' + " for design inquiries.";
+document.getElementById("theFooter").innerHTML = d.getFullYear() + "  The site is by Ann-Marie Foerster.  " + '<a itemprop="url" href="mailto:' + emailE + '">' + 'Contact me' + '</a>' + " for design inquiries.";
 
-document.getElementById("backupContact").innerHTML = '<a href="mailto:' + emailE + '">' + "Send me a direct email!" + '</a>';
+document.getElementById("backupContact").innerHTML = '<a itemprop="url" href="mailto:' + emailE + '">' + "Send me a direct email!" + '</a>';
 
 
 function respFunction() {
