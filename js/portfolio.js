fdaea84bf225410ae7742a8fb8ceead9e99b2073
Annie4Star <annmarie.foerster@gmail.com>
2020-01-30 22:44:54 -0500

    Fixed javascript FINALLY!

--- js/portfolio.js
+++ js/portfolio.js
@@ -9,11 +9,11 @@ function coverMail() {
 	emailE=('admin' + '@' + emailE)
 }
 
-document.getElementByID('NameObfuse').innerHTML = '<a href="mailto:' + emailE + '">Ann Foerster</a>';
+document.getElementById("NameObfuse").innerHTML = "<a href='mailto:" + emailE + "'>Ann Foerster</a>";
 
-document.getElementById("EnvelopeObfuse").innerHTML = '<a href="mailto:' + emailE + '"><img src="icons/studicons-mail.png" class="lighten" alt="Email" title="Email" title="Email"></a>';
+document.getElementById("EnvelopeObfuse").innerHTML = "<a href='mailto:" + emailE + "'><img src='icons/studicons-mail.png' class='lighten' alt='Email' title='Email' title='Email'></a>";
 
-document.getElementById("backupContact").innerHTML = 'You can always <a href="mailto:' + onload.coverMail() + '">email me here</a> if you are having issues with the form, or prefer email.';
+document.getElementById("backupContact").innerHTML = "You can always <a href='mailto:" + emailE + "'>email me here</a> if you are having issues with the form, or prefer email.";
 
 function respFunction() {
     var x = document.getElementById("mobilelinks");
