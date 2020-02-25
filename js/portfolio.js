8f72d5bcf9c1a01beb324e069a40e6c52250e334
Annie4Star <annmarie.foerster@gmail.com>
2020-02-24 20:31:12 -0500

    icons and codepen

--- js/portfolio.js
+++ js/portfolio.js
@@ -11,7 +11,7 @@ function coverMail() {
 
 document.getElementById("NameObfuse").innerHTML = "<a href='mailto:" + emailE + "'>Ann Foerster</a>";
 
-document.getElementById("EnvelopeObfuse").innerHTML = "<a href='mailto:" + emailE + "'><img src='icons/studicons-mail.png' class='lighten' alt='Email' title='Email' title='Email'></a>";
+document.getElementById("EnvelopeObfuse").innerHTML = "<a href='mailto:" + emailE + "'><i class='fas fa-envelope' alt='Email'></i></a>";
 
 document.getElementById("backupContact").innerHTML = "You can always <a href='mailto:" + emailE + "'>email me here</a> if you are having issues with the form, or prefer email.";
 
